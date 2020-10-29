e20de71a6029f0fd840321a95c0944f15dde6e09
Pavel Malinov <pmmalinov01@gmail.com>
2020-10-29 09:16:59 +0200

    smal fix to bootstrap and calico

--- pavel-malinov/K8S-networking/bootstrap.sh
+++ pavel-malinov/K8S-networking/bootstrap.sh
@@ -8,7 +8,7 @@ docker pull korvus/debian10
 
 footloose -c footloose-calico.yaml create
 
-footloose ssh root@node0 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true /root/install-k3s.sh"
+footloose ssh root@node0 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true /root/install-k3s.sh --flannel-backend=none --cluster-cidr=192.168.0.0/16 --disable=traefik"
 env k3stoken=$(footloose ssh root@node0 -- cat /var/lib/rancher/k3s/server/node-token)
 footloose ssh root@node1 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env K3S_URL=https://node0:6443 env K3S_TOKEN=$k3stoken /root/install-k3s.sh"
 footloose ssh root@node2 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env K3S_URL=https://node0:6443 env K3S_TOKEN=$k3stoken /root/install-k3s.sh"
