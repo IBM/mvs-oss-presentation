d3e2c015de807dcde1378e59b212958139f93409
pmmalinov01 <59862813+pmmalinov01@users.noreply.github.com>
2020-10-30 09:15:51 +0200

    K8s networking (#5)
    * adding gitignore
    
    * moved gitignore to the proper place
    
    * init commit for k8s networking
    
    * trying to set up gitignore
    
    * prepending double asterisk
    
    * moved .gitignore up
    
    * Renamed flannel to calico and added manifest for the CNI
    
    * smal fix to bootstrap and calico
    
    * fix bootstrap script by passing the footloose config
    
    * Removed option to disable traefk it is not accepted by the script
    
    * added presentation
    
    * finaly ignoring cluster key

---
+++ pavel-malinov/K8S-networking/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+set -x
+
+docker network create footloose-cluster
+
+docker pull korvus/debian10:0.6.3
+
+footloose -c footloose-calico.yaml create
+
+footloose -c footloose-calico.yaml ssh root@node0 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true /root/install-k3s.sh --flannel-backend=none --cluster-cidr=192.168.0.0/16"
+export k3stoken=$(footloose -c footloose-calico.yaml ssh root@node0 -- cat /var/lib/rancher/k3s/server/node-token)
+footloose -c footloose-calico.yaml ssh root@node1 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env K3S_URL=https://node0:6443 env K3S_TOKEN=$k3stoken /root/install-k3s.sh"
+footloose -c footloose-calico.yaml ssh root@node2 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env K3S_URL=https://node0:6443 env K3S_TOKEN=$k3stoken /root/install-k3s.sh"
+
+
